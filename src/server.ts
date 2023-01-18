b3c83c128894c4167485400f89d3a7f836619596
Tiago Rodrigues <tiagoasrodrigues@protonmail.com>
2023-01-18 13:46:54 -0300

    :wrench: Setup prisma

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
-// Initial project
+import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany()
+
+  return habits
+})
+
+app.listen({
+  port: 4444
+}).then((port) => {
+  console.log(`HTTP Server running in: ${port}`);
+})
